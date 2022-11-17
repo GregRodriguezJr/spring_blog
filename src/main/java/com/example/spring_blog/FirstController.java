b6700a3bb0ab2cddd99e6630ca5e0c7ecee22078
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-17 15:32:59 -0600

    made and tested controller

---
+++ src/main/java/com/example/spring_blog/FirstController.java
@@ -0,0 +1,20 @@
+package com.example.spring_blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Hello world from spring";
+    }
+
+    @RequestMapping (method = RequestMethod.GET, path = "/codeup")
+    @ResponseBody
+    public String codeup() {
+        return "Hello world from codeup";
+    }
+}
