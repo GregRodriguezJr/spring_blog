ed60ac809024969cb9549dbe585d4c7bc6ce1b93
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-18 16:21:40 -0600

    created controllers dir, created bootstrap fragment for CDNs

--- src/main/java/com/example/spring_blog/controllers/PostController.java
+++ src/main/java/com/example/spring_blog/controllers/PostController.java
@@ -1,4 +1,4 @@
-package com.example.spring_blog;
+package com.example.spring_blog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
