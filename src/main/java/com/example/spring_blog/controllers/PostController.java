a965bfcbf1198354c164b1113f76b7119acda60e
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-19 18:50:36 -0600

    made objects for arraylist of posts and added attribute for model

--- src/main/java/com/example/spring_blog/controllers/PostController.java
+++ src/main/java/com/example/spring_blog/controllers/PostController.java
@@ -5,11 +5,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    public String AllPosts() {
+    public String AllPosts(Model model) {
+
+        //Instantiate list to hold all Post objects
+        ArrayList <Post> allPosts = new ArrayList<>();
+
+        // Instantiate new Post objects
+        Post post1 = new Post(1, "Top Destinations", "Paris, London, Hong Kong,..." );
+        Post post2 = new Post(2, "The day in the life of a dev", "code, debug, code, debug");
+        Post post3 = new Post(3, "VsCode vs Intellij", "Pros and cons of both...");
+
+        // Add objects to arraylist
+        allPosts.add(post1);
+        allPosts.add(post2);
+        allPosts.add(post3);
+
+        // Add the list of Posts as a model attribute to send to the template for rendering
+        model.addAttribute("allPost", allPosts);
+
+        // Return the template
         return "posts/index";
     }
 
