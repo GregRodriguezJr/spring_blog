7ce4485372627ef493d9a41aa20bc4d719e83116
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-19 18:22:27 -0600

    tested add attribute to view from controller

--- src/main/java/com/example/spring_blog/controllers/PostController.java
+++ src/main/java/com/example/spring_blog/controllers/PostController.java
@@ -1,6 +1,7 @@
 package com.example.spring_blog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -12,7 +13,8 @@ public class PostController {
     }
 
     @GetMapping("/posts/{id}")
-    public String post() {
+    public String post(@PathVariable long id, Model model) {
+        model.addAttribute("postId", id);
         return "show";
     }
 
