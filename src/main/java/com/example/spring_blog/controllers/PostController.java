0d54b231b38f3524715fff7b64edbcc389b9dca9
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-21 15:42:22 -0600

    updated post mapping to pass object instead of hardcoded values

--- src/main/java/com/example/spring_blog/controllers/PostController.java
+++ src/main/java/com/example/spring_blog/controllers/PostController.java
@@ -1,7 +1,9 @@
 package com.example.spring_blog.controllers;
 
 import com.example.spring_blog.models.Post;
+import com.example.spring_blog.models.User;
 import com.example.spring_blog.repositories.PostRepository;
+import com.example.spring_blog.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +15,11 @@ import java.util.List;
 public class PostController {
 
     private final PostRepository postDao;
+    private final UserRepository userDao;
 
-    public PostController(PostRepository postDao) {
+    public PostController(PostRepository postDao, UserRepository userDao) {
         this.postDao = postDao;
+        this.userDao = userDao;
     }
 
     @GetMapping("/posts")
@@ -33,11 +37,8 @@ public class PostController {
 
     @GetMapping("/posts/{id}")
     public String post(@PathVariable long id, Model model) {
-
-        Post myPost = new Post(id,"Five potluck ideas", "example 1, example 2...");
-
-        model.addAttribute("myPost", myPost);
-        model.addAttribute("postId", id);
+        Post singlePost = postDao.getById(id);
+        model.addAttribute("post", singlePost);
         return "posts/show";
     }
 
@@ -51,6 +52,8 @@ public class PostController {
         Post post = new Post();
         post.setTitle(title);
         post.setBody(body);
+        User user = userDao.getById(1L);
+        post.setUser(user);
         postDao.save(post);
         return "redirect:/posts";
     }
